27e9106e63ad5740dc23d9e2409a48ea28917d92
Tommy Chanthavisouk <wildcodeschool.sprinkler515@pm.me>
2024-04-18 16:06:13 +0200

    create main program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _0418_S4_Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> dictionary = [];
+
+            dictionary['a'] = "Alaska";
+            dictionary['b'] = "Belgium";
+            dictionary['c'] = "China";
+            DisplayDict(dictionary);
+
+            Console.WriteLine("\nRemoving key 'c':");
+            dictionary.Remove('c');
+            DisplayDict(dictionary);
+        }
+
+        static void DisplayDict(Dictionary<char, string> dictionary)
+        {
+            foreach (KeyValuePair<char, string> kvp in dictionary)
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+    }
+}
